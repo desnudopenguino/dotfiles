d2047a6b8710fdbeb42f7f517613ca7a9914848b
Bucky Townsend <desnudopenguino@gmail.com>
2015-10-30 01:07:48 -0700

    Added Tmux
    Added soft link for tmux config

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
-!#/bin/sh
+#!/bin/sh
 
+
+# soft link vim config (.vimrc)
+ln -sf `pwd`/.vimrc ~/.vimrc
+
+# soft link vim directory (.vim/)
+ln -sf `pwd`/.vim ~/.vim
+
+# soft link tmux config (.tmux.conf)
+ln -sf `pwd`/.tmux.conf ~/.tmux.conf

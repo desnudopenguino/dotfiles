7079efe790d8e807e5f306c914f9f2da138d62b6
Bucky <bucky@bart.hsd1.wa.comcast.net>
2015-11-10 07:18:24 -0700

    Added .tmux directory linking

--- install.sh
+++ install.sh
@@ -10,4 +10,7 @@ ln -sf `pwd`/.vim ~/.vim
 # soft link tmux config (.tmux.conf)
 ln -sf `pwd`/.tmux.conf ~/.tmux.conf
 
+# soft link tmux directory
+ln -sf `pwd`/.tmux ~/.tmux
+
 ln -sf `pwd`/.Xdefaults ~/.Xdefaults

1e8dda736327241fe176eb796400dbc4897d3f54
Bucky <bucky@bart.hsd1.wa.comcast.net>
2015-11-16 08:20:00 -0700

    added setting up magic for i3

--- install.sh
+++ install.sh
@@ -35,11 +35,18 @@ else
 	echo "..Xdefaults already exists..."
 fi
 
-# soft link vim directory (.vim/)
+# soft link fluxbox directory (.fluxbox/)
 if [ ! -h ~/.fluxbox ]; then
 	ln -sf `pwd`/.fluxbox ~/.fluxbox
 else
 	echo ".fluxbox/ already exists..."
+fi
+
+# soft link i3 directory (.i3/)
+if [ ! -h ~/.i3 ]; then
+	ln -sf `pwd`/.i3 ~/.i3
+else
+	echo ".i3/ already exists..."
 fi
 
 echo "Downloading Submodules..."
